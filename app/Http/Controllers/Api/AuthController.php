e771cd2d11b217164b1e626b1821efd25c2f5656
idstck <idstackdeveloper@gmail.com>
2018-08-29 17:00:26 +0700

    Create login function with jwt

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Auth\RegisterRequest;
 use App\Models\User;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Carbon\Carbon;
 
 class AuthController extends Controller
 {
@@ -16,5 +19,26 @@ class AuthController extends Controller
             'email' => $request->json('email'),
             'password' => bcrypt($request->json('password'))
         ]);
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            $token = JWTAuth::attempt($request->only('email', 'password'), [
+                'exp' => Carbon::now()->addWeek()->timestamp,
+            ]);
+        } catch (JWTException $e) {
+            return response()->json([
+                'error' => 'Could not authenticate',
+            ], 500);
+        }
+
+        if (!$token) {
+            return response()->json([
+                'error' => 'Could not authenticate',
+            ], 401);
+        }
+
+        return response()->json(compact('token'));
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,11 @@
 
 Route::group(['prefix' => 'v1', 'namespace' => 'Api'], function () {
     Route::post('/auth/register', 'AuthController@register');
+    Route::post('/auth/login', 'AuthController@login');
+
+    Route::group(['middleware' => 'jwt.auth'], function () {
+        Route::get('/test', function () {
+            return 'Success';
+        });
+    });
 });
